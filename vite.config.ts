2e89beef945acc3554406686e366b298d9430413
startoverrox <toplondon08@gmail.com>
2025-07-31 16:32:14 +0900

    chore: package install (motion, storybook )

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,41 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import svgr from 'vite-plugin-svgr';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import path from 'path';
+/// <reference types="vitest/config" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 // https://vite.dev/config/
+import { fileURLToPath } from "node:url";
+import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
+const dirname =
+  typeof __dirname !== "undefined"
+    ? __dirname
+    : path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr(), tsconfigPaths()],
   server: {
     port: 3002,
     proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
+      "/api": {
+        target: "http://localhost:8000",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-
+        rewrite: (path) => path.replace(/^\/api/, ""),
         configure: (proxy, options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('error', err);
+          proxy.on("error", (err, _req, _res) => {
+            console.log("error", err);
           });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Request sent to target:', req.method, req.url);
+          proxy.on("proxyReq", (proxyReq, req, _res) => {
+            console.log("Request sent to target:", req.method, req.url);
           });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
+          proxy.on("proxyRes", (proxyRes, req, _res) => {
             console.log(
-              'Response received from target:',
+              "Response received from target:",
               proxyRes.statusCode,
-              req.url
+              req.url,
             );
           });
         },
@@ -36,7 +44,35 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
+  },
+  test: {
+    projects: [
+      {
+        extends: true,
+        plugins: [
+          // The plugin will run tests for the stories defined in your Storybook config
+          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+          storybookTest({
+            configDir: path.join(dirname, ".storybook"),
+          }),
+        ],
+        test: {
+          name: "storybook",
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: "playwright",
+            instances: [
+              {
+                browser: "chromium",
+              },
+            ],
+          },
+          setupFiles: [".storybook/vitest.setup.ts"],
+        },
+      },
+    ],
   },
 });
