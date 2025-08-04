d8df102f4dd0a3255fa79d7844a1822f471eb959
startoverrox <toplondon08@gmail.com>
2025-08-04 16:59:37 +0900

    rename: RegisterPage -> SignupPage

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import DefaultLayout from "@/routes/layouts/DefaultLayout";
 import HomePage from "@/routes/pages/HomePage";
 import JobPage from "@/routes/pages/JobPage";
 import LoginPage from "@/routes/pages/LoginPage";
-import RegisterPage from "@/routes/pages/RegisterPage";
+import SignupPage from "@/routes/pages/SignupPage";
 import ErrorPage from "@/routes/pages/ErrorPage";
 
 const router = createBrowserRouter([
@@ -40,7 +40,7 @@ const router = createBrowserRouter([
       },
       {
         path: "/signup",
-        element: <RegisterPage />,
+        element: <SignupPage />,
       },
     ],
   },
