5b4aac8ed012e9822a718b64dd83c1c4dc42beeb
startoverrox <toplondon08@gmail.com>
2025-08-04 14:38:31 +0900

    feat: add LoginPage

--- src/routes/layouts/AuthLayout.tsx
+++ src/routes/layouts/AuthLayout.tsx
@@ -2,9 +2,9 @@ import { Outlet } from "react-router";
 
 const AuthLayout = () => {
   return (
-    <>
+    <div className="flex h-[100vh] items-center justify-center">
       <Outlet />
-    </>
+    </div>
   );
 };
 
--- src/routes/pages/LoginPage.tsx
+++ src/routes/pages/LoginPage.tsx
@@ -1,5 +1,22 @@
+import { useNavigate } from "react-router";
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const navigate = useNavigate();
+  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("login");
+  };
+
+  return (
+    <div className="flex h-100 w-100 flex-col items-center justify-center gap-4 border [&_button]:border [&_input]:border">
+      <form onSubmit={handleLogin} className="flex flex-col gap-4">
+        <input type="text" placeholder="email" />
+        <input type="password" placeholder="password" />
+        <button type="submit">Login</button>
+      </form>
+      <button onClick={() => navigate("/signup")}>Signup</button>
+    </div>
+  );
 };
 
 export default LoginPage;
