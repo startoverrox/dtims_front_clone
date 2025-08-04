c026b7281583627025d40cfea7b7477fe3ca14c3
startoverrox <toplondon08@gmail.com>
2025-08-04 17:03:47 +0900

    feat: add SignupPage

--- src/routes/pages/LoginPage.tsx
+++ src/routes/pages/LoginPage.tsx
@@ -2,16 +2,21 @@ import { useNavigate } from "react-router";
 
 const LoginPage = () => {
   const navigate = useNavigate();
+
   const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("login");
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    console.log("login", email, password);
+    navigate("/");
   };
 
   return (
     <div className="flex h-100 w-100 flex-col items-center justify-center gap-4 border [&_button]:border [&_input]:border">
       <form onSubmit={handleLogin} className="flex flex-col gap-4">
-        <input type="text" placeholder="email" />
-        <input type="password" placeholder="password" />
+        <input type="text" name="email" placeholder="email" />
+        <input type="password" name="password" placeholder="password" />
         <button type="submit">Login</button>
       </form>
       <button onClick={() => navigate("/signup")}>Signup</button>
--- src/routes/pages/SignupPage.tsx
+++ src/routes/pages/SignupPage.tsx
@@ -1,5 +1,26 @@
+import { useNavigate } from "react-router";
+
 const SignupPage = () => {
-  return <div>SignupPage</div>;
+  const navigate = useNavigate();
+
+  const handleSignup = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    console.log("signup", email, password);
+    navigate("/login");
+  };
+
+  return (
+    <div className="flex h-100 w-100 flex-col items-center justify-center gap-4 border [&_button]:border [&_input]:border">
+      <form onSubmit={handleSignup} className="flex flex-col gap-4">
+        <input type="text" name="email" placeholder="email" />
+        <input type="password" name="password" placeholder="password" />
+        <button type="submit">Signup</button>
+      </form>
+    </div>
+  );
 };
 
 export default SignupPage;
